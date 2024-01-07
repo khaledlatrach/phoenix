801e59d2fd2bfeb311a083017272d16f657b3fb6
khaled latrach <khaledlatrach4@gmail.com>
2024-01-07 21:25:10 +0100

    Initial commit

---
+++ lib/myapp_web/controllers/page_controller.ex
@@ -0,0 +1,14 @@
+defmodule MyappWeb.PageController do
+  use MyappWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+
+  def users(conn, _params) do
+    IO.puts("khalid")
+    render(conn, :users, layout: false)
+  end
+end
